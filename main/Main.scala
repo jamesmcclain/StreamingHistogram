6a1661bf91a9159f6def42c1ff8f6cadf718867c
James McClain <jmcclain@azavea.com>
2016-01-25 17:22:28 -0500

    New Main Function To Exercise Better Performance

--- main/Main.scala
+++ main/Main.scala
@@ -3,17 +3,16 @@ package com.daystrom_data_concepts
 import scala.util.Random
 
 object Main {
-  val limit = 1000000
-  val size = 1000
+  val limit = 10000000
+  val size = 500
   val r = Random
   val sh = new StreamingHistogram(size)
 
   def main(args: Array[String]) = {
-    var control = 0.0
-
     Iterator.range(0,limit).foreach({ i => sh.update(r.nextGaussian) })
     println
-    // println(s"buckets=${sh.getBuckets} \ndeltas=${sh.getDeltas}")
-    // println(s"${sh.ping}")
+    println(s"${sh.getBuckets.size} buckets=${sh.getBuckets}")
+    println
+    println(s"${sh.getDeltas.size} deltas=${sh.getDeltas}")
   }
 }
