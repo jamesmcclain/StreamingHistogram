bb114a85f7c0045809a180292c0d04a42b19157a
James McClain <jmcclain@azavea.com>
2016-01-26 12:09:19 -0500

    Combine Histograms

--- histogram/StreamingHistogram.scala
+++ histogram/StreamingHistogram.scala
@@ -3,25 +3,42 @@ package com.daystrom_data_concepts
 import java.util.TreeMap
 import java.util.Comparator
 import scala.collection.JavaConverters._
+import StreamingHistogram.{BucketType, DeltaType}
 
 
-class StreamingHistogram(m: Int) {
+object StreamingHistogram {
   private type BucketType = (Double, Int)
   private type DeltaType = (Double, BucketType, BucketType)
 
+  def apply(m: Int) = new StreamingHistogram(m, None, None)
+
+  def apply(m: Int, buckets: TreeMap[Double, Int], deltas: TreeMap[DeltaType, Unit]) =
+    new StreamingHistogram(m, Some(buckets), Some(deltas))
+}
+
+/**
+ * Ben-Haim, Yael, and Elad Tom-Tov. "A streaming parallel decision
+ * tree algorithm."  The Journal of Machine Learning Research 11
+ * (2010): 849-872.
+ */
+class StreamingHistogram(
+  m: Int,
+  startingBuckets: Option[TreeMap[Double, Int]],
+  startingDeltas: Option[TreeMap[DeltaType, Unit]]
+) {
   class DeltaCompare extends Comparator[DeltaType] {
     def compare(a: DeltaType, b: DeltaType): Int =
       if (a._1 < b._1) -1
       else if (a._1 > b._1) 1
-      else 0
+      else {
+        if (a._2._1 < b._2._1) -1
+        else if (a._2._1 > b._2._1) 1
+        else 0
+      }
   }
 
-  private val buckets = new TreeMap[Double, Int]
-  private val deltas = new TreeMap[DeltaType, Unit](new DeltaCompare)
-
-  def getBuckets(): List[BucketType] = buckets.asScala.toList
-
-  def getDeltas(): List[DeltaType] = deltas.keySet.asScala.toList
+  private val buckets = startingBuckets.getOrElse(new TreeMap[Double, Int])
+  private val deltas = startingDeltas.getOrElse(new TreeMap[DeltaType, Unit](new DeltaCompare))
 
   /**
    * Take two buckets and return their composite.
@@ -38,6 +55,11 @@ class StreamingHistogram(m: Int) {
    * Before: left ----- middle1 ----- middle2 ----- right
    *
    * After: left ------------- middle ------------- right
+   *
+   * This function appropriately modifies both the buckets and the
+   * deltas.  The deltas on either side of the collapsed pair are
+   * removed and replaced with deltas meed the mid-point and
+   * respective extremes.
    */
   private def merge(): Unit = {
     val delta = deltas.firstKey
@@ -79,25 +101,21 @@ class StreamingHistogram(m: Int) {
     buckets.put(middle._1, middle._2)
   }
 
-  /**
-   * Add a datum.
-   */
-  def update(d: Double): Unit = {
+  def update(b: BucketType): Unit = {
     /* First entry */
-    if (buckets.size == 0) buckets.put(d, 1)
+    if (buckets.size == 0)
+      buckets.put(b._1, b._2)
     /* Duplicate entry */
-    else if (buckets.containsKey(d)) {
-      val count = buckets.get(d)
-      buckets.put(d, count + 1)
-    }
+    else if (buckets.containsKey(b._1))
+      buckets.put(b._1, buckets.get(b._1) + b._2)
     /* Create new entry */
     else {
       val smaller = {
-        val entry = buckets.lowerEntry(d)
+        val entry = buckets.lowerEntry(b._1)
         if (entry != null) Some(entry.getKey, entry.getValue); else None
       }
       val larger = {
-        val entry = buckets.higherEntry(d)
+        val entry = buckets.higherEntry(b._1)
         if (entry != null) Some(entry.getKey, entry.getValue); else None
       }
 
@@ -112,19 +130,44 @@ class StreamingHistogram(m: Int) {
       /* Add delta between new bucket and next-largest bucket */
       if (larger != None) {
         val large = larger.get
-        val delta = large._1 - d
-        deltas.put((delta, (d, 1), large), Unit)
+        val delta = large._1 - b._1
+        deltas.put((delta, b, large), Unit)
       }
 
       /* Add delta between new bucket and next-smallest bucket */
       if (smaller != None) {
         val small = smaller.get
-        val delta = d - small._1
-        deltas.put((delta, small, (d, 1)), Unit)
+        val delta = b._1 - small._1
+        deltas.put((delta, small, b), Unit)
       }
-
-      buckets.put(d, 1)
-      if (buckets.size > m) merge
     }
+
+    buckets.put(b._1, b._2)
+    if (buckets.size > m) merge
   }
+
+  /**
+   * Combine operator.
+   */
+  def +(other: StreamingHistogram): StreamingHistogram = {
+    val sh = StreamingHistogram(this.m, this.buckets, this.deltas)
+    sh.update(other.getBuckets)
+    sh
+  }
+
+  def getQuantile(q: Double): Double = 33
+
+  def getQuantiles(k: Int) = List.range(0,k).map(_ / k.toDouble).map(getQuantile)
+
+  def update(d: Double): Unit = update((d, 1))
+
+  def update(bs: Seq[BucketType]): Unit =
+    bs.foreach({ b => update(b) })
+
+  def update(ds: Seq[Double])(implicit dummy: DummyImplicit): Unit =
+    ds.foreach({ d => update((d, 1)) })
+
+  def getBuckets(): List[BucketType] = buckets.asScala.toList
+
+  def getDeltas(): List[DeltaType] = deltas.keySet.asScala.toList
 }
