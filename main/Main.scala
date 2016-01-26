82608a2b0ecbabe47ced9d3c4002d0b1442f5c38
James McClain <jmcclain@azavea.com>
2016-01-26 12:35:26 -0500

    Expand Main Program
       * Exericse Ability to Combine Histograms
       * Exercise Quantiles

--- main/Main.scala
+++ main/Main.scala
@@ -3,16 +3,27 @@ package com.daystrom_data_concepts
 import scala.util.Random
 
 object Main {
-  val limit = 10000000
-  val size = 500
+  val limit = 1000000
+  val size = 80
   val r = Random
-  val sh = new StreamingHistogram(size)
+  val rs1 = Array.ofDim[Double](limit)
+  val rs2 = Array.ofDim[Double](limit)
+  val sh1 = StreamingHistogram(size)
+  val sh2 = StreamingHistogram(size)
 
   def main(args: Array[String]) = {
-    Iterator.range(0,limit).foreach({ i => sh.update(r.nextGaussian) })
-    println
-    println(s"${sh.getBuckets.size} buckets=${sh.getBuckets}")
-    println
-    println(s"${sh.getDeltas.size} deltas=${sh.getDeltas}")
+    var i = 0
+
+    while (i < limit) {
+      rs1(i) = r.nextGaussian
+      rs2(i) = r.nextGaussian
+      i += 1
+    }
+
+    sh1.update(rs1)
+    sh2.update(rs2)
+    val sh3 = sh1 + sh2
+    println(s"quantiles = ${sh3.getQuantiles(10)}")
+    println(s"75th percentile = ${sh3.getQuantile(0.75)}")
   }
 }
